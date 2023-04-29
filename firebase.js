96b5b232ee17b3513622f5ff542adc14e7e1e548
Arnix Minadeo <arnix676@gmail.com>
2023-04-28 23:12:35 -0400

    Add change in firebase file

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
+import { getAuth , createUserWithEmailAndPassword  } from "firebase/auth";
 import {
   FIREBASE_API_KEY,
   FIREBASE_AUTH_DOMAIN,
@@ -30,4 +30,5 @@ console.log("funciona");
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
+
 //const analytics = getAnalytics(app);
